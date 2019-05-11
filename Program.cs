7ec464c906c23b41b8d231e61e8c4f5a8d695401
Markus Gehrig <markus.gehrig96@gmail.com>
2019-05-11 10:02:24 +0200

    [FEATURE] Add Constructor which only need gewicht and laenge.

--- Program.cs
+++ Program.cs
@@ -38,11 +38,27 @@ namespace PKW_20190511
         /// <param name="t_gewicht">Gewicht des Autos</param>
         /// <param name="t_sitze">Anzahl Sitze des Autos</param>
         public PKW(double t_leange, int t_gewicht, int t_sitze)
+            :this(t_leange, t_gewicht)
         {
-            gewicht         = t_gewicht;
-            laenge          = t_leange;
-            anzahl_sitze    = t_sitze;
+            Gewicht         = t_gewicht;
+            Laenge          = t_leange;
+            Anzahl_sitze    = t_sitze;
         }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="t_leange">Länge des Autos</param>
+        /// <param name="t_gewicht">Gewicht des Autos</param>
+        public PKW(double t_leange, int t_gewicht)
+        {
+            Gewicht = t_gewicht;
+            Laenge = t_leange;
+        }
+
+        public double Laenge { get => laenge; set => laenge = value; }
+        public int Gewicht { get => gewicht; set => gewicht = value; }
+        public int Anzahl_sitze { get => anzahl_sitze; set => anzahl_sitze = value; }
 
         /// <summary>
         /// 
@@ -51,9 +67,6 @@ namespace PKW_20190511
         {
 
         }
-
-
-
     }
 
     class Program
@@ -63,6 +76,15 @@ namespace PKW_20190511
             // Instantiieren Autos
             // Auto BMW
             PKW myBMW = new PKW(5.20, 700, 5);
+            Console.WriteLine("Gewicht:" + myBMW.Gewicht);
+
+            myBMW.Gewicht = 750;
+            Console.WriteLine("Gewicht:" + myBMW.Gewicht);
+
+            PKW myMaserati = new PKW(6.1, 1050);
+            Console.WriteLine("Gewicht:" + myMaserati.Gewicht);
+
+            Console.ReadKey();
         }
     }
 }
